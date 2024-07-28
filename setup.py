9ed32d281a673479caa7f6f2b99113e7cbc3c851
Chen Liu <chen.liu.cl2482@yale.edu>
2024-07-27 19:24:35 -0700

    update version

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open('README.md') as f:
 
 setup(
     name='citation-map',
-    version='3.2',
-    license='MIT',
+    version='3.3',
+    license='Apache 2.0',
     author='Chen Liu',
     author_email='chen.liu.cl2482@yale.edu',
     packages={'citation_map'},
@@ -20,7 +20,7 @@ setup(
     classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: Apache Software License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     ],
 )
