5704f2bd2187977a4d03dfa8983dcccbe97cce2f
Chen Liu <chen.liu.cl2482@yale.edu>
2024-07-27 19:22:04 -0700

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='citation-map',
-    version='3.1',
+    version='3.2',
     license='MIT',
     author='Chen Liu',
     author_email='chen.liu.cl2482@yale.edu',
