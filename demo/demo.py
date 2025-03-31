f59d543008f2625fe197dbc12ac86aa5680165a7
Caitao Zhan <caitaozhan@gmail.com>
2025-03-30 19:37:15 -0500

    1) add check_corner_case() to the affiliation name to prevent cases such as putting Amazon in Brazil Amazon forest; 2) update logic of dealing with NO_AUTHOR_FOUND_STR in affiliation_text_to_geocode() and also removing wrong affiliations, might have a little overlap with the affiliation_conservative option; 3) add sorted() in step 4 coordinates_and_info; 4) add parse_csv parameter in the demo code; 5) update .gitignore

--- demo/demo.py
+++ demo/demo.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     # This is my Google Scholar ID. Replace this with your ID.
     scholar_id = '3rDjnykAAAAJ'
     generate_citation_map(scholar_id, output_path='citation_map.html',
-                          cache_folder='cache', affiliation_conservative=False, num_processes=16,
+                          cache_folder='cache', parse_csv=False, affiliation_conservative=False, num_processes=16,
                           use_proxy=False, pin_colorful=True, print_citing_affiliations=True)
